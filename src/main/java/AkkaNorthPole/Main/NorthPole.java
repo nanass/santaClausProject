130339e40ca0f89b6d9c092097c1331361d71949
nathan <nathan.nass@gmail.com>
2013-10-27 10:49:35 -0500

    Akka North Pole Cleanup

--- src/main/java/AkkaNorthPole/Main/NorthPole.java
+++ src/main/java/AkkaNorthPole/Main/NorthPole.java
@@ -9,10 +9,35 @@ import AkkaNorthPole.Messages.NorthPoleMsg;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import org.nettosphere.samples.chat.NettoServer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NorthPole {
+enum name{ Dasher, Dancer, Prancer, Vixen, Comet, Cupid, Donder, Blitzen, Ruldolph }
 
+class NorthPole{
+    public static void main(String[] args) throws IOException {
+
+        NettoServer ns = new NettoServer();
+
+        List<ActorRef> elves = new ArrayList<ActorRef>();
+        List<ActorRef> reindeer = new ArrayList<ActorRef>();
+
+        ActorSystem system = ActorSystem.create("NorthPole");
+
+        ActorRef santa = system.actorOf(Props.create(Santa.class, "Santa"));
+        ActorRef secretary = system.actorOf(Props.create(WaitingRoom.class, "WaitingRoom", santa));
+        santa.tell(new Msg(NorthPoleMsg.Secretary), secretary);
+
+        for (int i = 1; i <= 10; i++){
+            elves.add(system.actorOf(Props.create(Elf.class, "Elf" + String.valueOf(i), secretary)));
+        }
+        for (name r : name.values()){
+            reindeer.add(system.actorOf(Props.create(Reindeer.class, r.toString(), secretary)));
+        }
+
+
+    }
 }
