9ada1805b0f00743da3e6c45cd17b343a310e4eb
nathan <nathan.nass@gmail.com>
2013-10-27 21:23:16 -0500

    started mrs claus

---
+++ src/main/java/AkkaNorthPole/Actors/MrsClaus.java
@@ -0,0 +1,11 @@
+package AkkaNorthPole.Actors;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nathan
+ * Date: 10/27/13
+ * Time: 10:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MrsClaus {
+}
